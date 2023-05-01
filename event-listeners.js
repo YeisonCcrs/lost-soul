ccc14faebb2ab310fef9a86970e1d01e0d4693c8
AdrianCCRS <yeadcato@gmail.com>
2023-04-30 23:59:31 -0500

    version alpha 1.0.1 (separacion de los listeners a otro js y edicioon mejorada de los movimientos hacia los lados)

---
+++ event-listeners.js
@@ -0,0 +1,23 @@
+window.addEventListener('keydown', (event) => {
+    console.log(event)
+    switch(event.key){
+        case 'w':  if(jugador.velocidad.y===0) jugador.velocidad.y=-15
+            break
+        case 'a': teclas.a.presionada=true
+            break
+        case 'd': teclas.d.presionada=true
+            break
+    }
+    
+})
+
+window.addEventListener('keyup', (event) => {
+    console.log(event)
+    switch(event.key){
+        case 'a': teclas.a.presionada=false
+            break
+        case 'd': teclas.d.presionada=false
+            break
+    }
+    
+})
